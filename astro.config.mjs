e2ca14b5b0dbc25c141eb11b382ccfa3e89c520a
lorenachevallot <loni.chevallot@gmail.com>
2025-01-24 11:58:32 +0100

    ajout éléments

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,14 @@ import tailwind from '@astrojs/tailwind';
 
 import netlify from '@astrojs/netlify';
 
+
+
+import alpinejs from '@astrojs/alpinejs';
+
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
   adapter: netlify()
 });
