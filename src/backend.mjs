788b5212b5a3cf591b319263f7539226ebbc2f4b
lorenachevallot <loni.chevallot@gmail.com>
2025-02-21 15:30:50 +0100

    avancement projet

--- src/backend.mjs
+++ src/backend.mjs
@@ -6,13 +6,15 @@ const pb = new PocketBase("http://127.0.0.1:8090");
 //Authentification
 
 export async function superUserauth() {
-    const authData = await pb.collection("_superusers").authWithPassword("admin@gmail.com", "ET2Sn9Qk2lRZsPv");
+    const authData = await pb.collection("_superusers").authWithPassword("admin@gmail.com", "PxJYAjp2tqfI5aS");
     return authData;
 }
 
 
+
 //fonction addOffre dans backend.mjs
 export async function addOffre(house) {
+    superUserauth();
     try {
         await pb.collection('maison').create(house);
         return {
@@ -44,6 +46,8 @@ export async function allMaisons() {
 //Exercice 11
 
 export async function oneID() {
+    await superUserauth();
+
     const record = await pb
         .collection("maison")
         .getOne("69dj9w2m56i4le7");
@@ -53,6 +57,8 @@ export async function oneID() {
 //Exercice 12
 
 export async function allMaisonsFavori() {
+    await superUserauth();
+
     const records = await pb
         .collection("maison")
         .getFullList({ filter: "favori = true" });
@@ -61,6 +67,8 @@ export async function allMaisonsFavori() {
 
 //Exercice 13
 export async function allMaisonsSorted() {
+    await superUserauth();
+
     const records = await pb
         .collection("maison")
         .getFullList({ sort: "prix" });
@@ -82,6 +90,7 @@ export async function bySurface(surface) {
 
 //Exercice 15
 export async function surfaceORprice(surface, p) {
+    await superUserauth();
     const records = await pb
         .collection("maison")
         .getFullList({ filter: `surface >= ${surface} OR prix <= ${p}` });
@@ -90,6 +99,7 @@ export async function surfaceORprice(surface, p) {
 
 // Exercice 19
 export async function getAgentById(id) {
+    await superUserauth();
     const record = await pb.collection("agent").getOne(id);
     return record;
 }
@@ -99,6 +109,7 @@ export async function getAgentById(id) {
 //Exercice 7
 
 export async function allMaisonsAgents() {
+    await superUserauth();
     const allRecords = await pb.collection("maison").getFullList({
         expand: "agent",
     });
@@ -108,8 +119,13 @@ export async function allMaisonsAgents() {
 //Exercice 8
 
 export async function allMaisonsByAgentId(agentId) {
-    const records = await pb.collection("maison").getFullList({
+    await superUserauth();
+    let records = await pb.collection("maison").getFullList({
         filter: `agent.id = '${agentId}'`,
+    });
+    records = records.map((maison) => {
+        maison.imgUrl = pb.files.getURL(maison, maison.image);
+        return maison;
     });
     return records;
 }
@@ -118,6 +134,7 @@ export async function allMaisonsByAgentId(agentId) {
 //Exercice 9
 
 export async function allMaisonsByAgentNom(agentNom) {
+    await superUserauth();
     const records = await pb.collection("maison").getFullList({
         filter: `agent.nom = '${agentNom}'`,
     });
@@ -127,6 +144,7 @@ export async function allMaisonsByAgentNom(agentNom) {
 //Exercice 10
 
 export async function allMaisonsSortedAgent() {
+    await superUserauth();
     const records = await pb.collection("maison").getFullList({
         sort: "agent.id",
         expand: "agent",
@@ -137,6 +155,7 @@ export async function allMaisonsSortedAgent() {
 //Exercice 11
 
 export async function bySurfaceAgent(surface, agentId) {
+    await superUserauth();
     const records = await pb.collection("maison").getFullList({
         filter: `surface > ${surface} && agent.id = '${agentId}'`,
         expand: "agent",
@@ -145,7 +164,6 @@ export async function bySurfaceAgent(surface, agentId) {
 }
 
 export async function byPrice(prix) {
-    await superUserauth();
     await superUserauth();
     let records = await pb.collection("maison").getFullList({
         filter: `prix < ${prix}`,
@@ -161,6 +179,7 @@ export async function byPrice(prix) {
 //Exercice 12
 
 export async function maisonFavoriAgent(agentId) {
+    await superUserauth();
     const records = await pb.collection("maison").getFullList({
         filter: `favori = true && agent.id = '${agentId}'`,
         expand: "agent",
@@ -171,6 +190,7 @@ export async function maisonFavoriAgent(agentId) {
 //TP5
 
 export async function addNewMaison(newmaison) {
+    await superUserauth();
     await pb.collection("maison").create(newmaison);
 }
 
@@ -178,18 +198,21 @@ export async function addNewMaison(newmaison) {
 // Etape 3 
 
 export async function addNewAgent(newAgent) {
+    await superUserauth();
     await pb.collection("agent").create(newAgent);
 }
 
 // Etape 4
 
 export async function deleteMaisonById(id) {
+    await superUserauth();
     await pb.collection("maison").delete(id);
 }
 
 // Etape 5
 
 export async function deleteAgentById(id) {
+    await superUserauth();
     await pb.collection("agent").delete(id);
 }
 
@@ -197,6 +220,7 @@ export async function deleteAgentById(id) {
 // Etape 6
 
 export async function updateMaisonById(id, data) {
+    await superUserauth();
     await pb.collection("maison").update(id, data);
 
 }
@@ -204,18 +228,21 @@ export async function updateMaisonById(id, data) {
 //Etape 7
 
 export async function updateAgentById(id, data) {
+    await superUserauth();
     await pb.collection("agent").update(id, data);
 
 }
 
 export async function getOffre(id) {
+    await superUserauth();
     try {
-        let data = await pb.collection('maison').getOne(id);
-        data.imageUrl = pb.files.getURL(data, data.image);
-        return data;
+        let records = await pb.collection('maison').getOne(id);
+        records.imageUrl = pb.files.getURL(records, records.image);
+        return records;
     } catch (error) {
         console.log('Une erreur est survenue en lisant la maison', error);
         return null;
+
     }
 }
 
@@ -240,8 +267,33 @@ export async function filterByPrix(prixMin, prixMax) {
     }
 }
 
+//
+export async function getAllAgents() {
+    superUserauth();
+    const records = await pb.collection("agent").getFullList();
+    return records;
+}
+// try {
+//     await pb.collection('maison').create(house);
+//     return {
+//         success: true,
+//         message: 'Offre ajoutée avec succès'
+//     };
+// } catch (error) {
+//     console.log('Une erreur est survenue en ajoutant la maison', error);
+//     return {
+//         success: false,
+//         message: 'Une erreur est survenue en ajoutant la maison'
+//     };
+// }
 
 
+
+//Fonction setFavori
+export async function setFavori(house) {
+    await superUserauth();
+    await pb.collection('maison').update(house.id, { favori: !house.favori });
+}
 
 
 
